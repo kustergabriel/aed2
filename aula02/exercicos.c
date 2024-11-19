f95a437c9e99bfcdb4a544792faedfd17072b88f
Gabriel Kuster <gabrielazevedo1812@gmail.com>
2024-11-19 17:26:52 -0300

    feitos os exercicos para entregar

--- aula02/exercicos.c
+++ aula02/exercicos.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 #define TAM_ALFABETO 26
@@ -12,37 +13,55 @@ trie_nodo_t *trie_cria_nodo(void); // cria um novo nó
 void trie_insere(trie_nodo_t *raiz, char *chave); // insere uma palavra na trie
 int trie_busca(trie_nodo_t *raiz, char *chave); // busca uma palavra na trie
 void trie_destroi(trie_nodo_t *raiz); // destroi a trie
-int buscar_palavra (trie_nodo_t *raiz,char *chave);
+int conta_nodos(trie_nodo_t *raiz);
+void imprime_todas_chaves(trie_nodo_t *raiz, char *palavra, int nivel);
 
 int main  () {
-
+    char sdhuf[100];
     trie_nodo_t* a = trie_cria_nodo();
     trie_insere (a, "macaco");
-    if (buscar_palavra(a, "macaco")) {
-        printf("A palavra 'macaco' foi encontrada.\n");
-    } else {
-        printf("A palavra 'macaco' não foi encontrada.\n");
-    }
+    trie_insere (a, "cagao");
+
+   int contador = conta_nodos(a);
+
+    imprime_todas_chaves(a, sdhuf, 0);
 
     trie_destroi(a);
 
     return 0;
 }
 
-// minha busca na trie
-int buscar_palavra (trie_nodo_t *raiz,char *chave) {
+void imprime_todas_chaves(trie_nodo_t *raiz, char *palavra, int nivel) {
     trie_nodo_t *atual = raiz;
-    for (int i = 0; chave[i] != '\0'; i++) {
-        int indice = chave [i] - 'a'; // NAO ENTENDI ESSA PARTE DE JEITO MANEIRA
-        if (atual->chave[indice] == NULL) {
-            return 0;
-        }
-        atual = atual->chave[indice];
+    if (raiz == NULL) {
+        return;
     }
-    return atual->eh_final_palavra;
+    if (atual->eh_final_palavra) {
+        palavra[nivel] = '\0';
+        printf ("%s\n", palavra);   
+        }
+    for (int i = 0; i < TAM_ALFABETO; i++) {
+        if (raiz->chave[i] != NULL) {
+            palavra[nivel] = i + 'a';
+            imprime_todas_chaves(atual->chave[i], palavra, nivel+1);
+        }
+    }
 }
 
+int conta_nodos(trie_nodo_t *raiz) {
+    int contador = 0;
+    if (raiz == NULL) {
+        printf ("vaziaaa"); 
+    trie_nodo_t *atual = raiz;
 
+    for (int i = 0; i <= TAM_ALFABETO; i++) {
+        if (atual->chave[i] != NULL) {
+            contador += conta_nodos(atual->chave[i]);
+        }
+    }
+    }
+    return 1 + contador;
+}
 
 trie_nodo_t *trie_cria_nodo(void){
     trie_nodo_t *novo_no = (trie_nodo_t *)malloc(sizeof(trie_nodo_t));
